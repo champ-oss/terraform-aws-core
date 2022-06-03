54ab626afe651965bd6c8e669e2bee7bc5b7e5d0
Justin Reeves <reeves122@gmail.com>
2022-06-03 10:10:14 -0500

    add aws_s3_bucket_public_access_block

--- s3.tf
+++ s3.tf
@@ -75,3 +75,10 @@ data "aws_iam_policy_document" "s3" {
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "this" {
+  bucket                  = aws_s3_bucket.this.id
+  block_public_acls       = true
+  block_public_policy     = true
+  restrict_public_buckets = true
+  ignore_public_acls      = true
+}
