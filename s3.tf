6491e03290392efd08fcda7e2c142b40356e2b05
Justin Reeves <reeves122@gmail.com>
2022-06-07 11:22:49 -0500

    update s3 to use separate resources

--- s3.tf
+++ s3.tf
@@ -1,27 +1,42 @@
 resource "aws_s3_bucket" "this" {
   bucket_prefix = "aws-lb-"
-  acl           = "private"
   force_destroy = !var.protect
   tags          = var.tags
 
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_s3_bucket_acl" "this" {
+  bucket = aws_s3_bucket.this.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
+  bucket = aws_s3_bucket.this.bucket
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
     }
   }
+}
 
-  lifecycle_rule {
-    enabled = true
+resource "aws_s3_bucket_versioning" "this" {
+  bucket = aws_s3_bucket.this.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
 
+resource "aws_s3_bucket_lifecycle_configuration" "this" {
+  bucket = aws_s3_bucket.this.id
+  rule {
+    id     = "expiration"
+    status = "Enabled"
     expiration {
       days = var.log_retention
     }
-  }
-
-  lifecycle {
-    create_before_destroy = true
   }
 }
 
