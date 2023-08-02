43a26f7cff58de603e6620040bcd851deb0c0f8c
Justin Reeves <reeves122@gmail.com>
2023-08-02 09:44:27 -0500

    update acm module

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -50,7 +50,7 @@ data "aws_subnets" "public" {
 }
 
 module "acm" {
-  source            = "github.com/champ-oss/terraform-aws-acm.git?ref=v1.0.111-28fcc7c"
+  source            = "github.com/champ-oss/terraform-aws-acm.git?ref=use-validation-arn"
   git               = local.git
   domain_name       = "${local.git}.${data.aws_route53_zone.this.name}"
   create_wildcard   = false
