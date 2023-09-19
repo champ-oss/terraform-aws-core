62d8ca00bea42e4a5bbab5f66f5f1a6dab80257b
Justin Reeves <reeves122@gmail.com>
2023-09-19 10:17:11 -0500

    add s3 depends on to help with conflicting conditional operation

--- s3.tf
+++ s3.tf
@@ -26,7 +26,8 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
 }
 
 resource "aws_s3_bucket_versioning" "this" {
-  bucket = aws_s3_bucket.this.id
+  depends_on = [aws_s3_bucket_public_access_block.this]
+  bucket     = aws_s3_bucket.this.id
   versioning_configuration {
     status = "Enabled"
   }
@@ -44,8 +45,9 @@ resource "aws_s3_bucket_lifecycle_configuration" "this" {
 }
 
 resource "aws_s3_bucket_policy" "this" {
-  bucket = aws_s3_bucket.this.id
-  policy = data.aws_iam_policy_document.s3.json
+  depends_on = [aws_s3_bucket_public_access_block.this]
+  bucket     = aws_s3_bucket.this.id
+  policy     = data.aws_iam_policy_document.s3.json
 }
 
 data "aws_elb_service_account" "this" {}
