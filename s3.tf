a35bdcd07340fecc400d37a1c60bf4959ca353e7
Justin Reeves <reeves122@gmail.com>
2023-09-19 10:32:51 -0500

    update s3 depends on to help with conflicting conditional operation

--- s3.tf
+++ s3.tf
@@ -45,9 +45,8 @@ resource "aws_s3_bucket_lifecycle_configuration" "this" {
 }
 
 resource "aws_s3_bucket_policy" "this" {
-  depends_on = [aws_s3_bucket_public_access_block.this]
-  bucket     = aws_s3_bucket.this.id
-  policy     = data.aws_iam_policy_document.s3.json
+  bucket = aws_s3_bucket.this.id
+  policy = data.aws_iam_policy_document.s3.json
 }
 
 data "aws_elb_service_account" "this" {}
@@ -96,6 +95,7 @@ data "aws_iam_policy_document" "s3" {
 }
 
 resource "aws_s3_bucket_public_access_block" "this" {
+  depends_on              = [aws_s3_bucket_policy.this]
   bucket                  = aws_s3_bucket.this.id
   block_public_acls       = true
   block_public_policy     = true
