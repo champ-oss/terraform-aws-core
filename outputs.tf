45961f5de39b1602c29d1477c8bf465ac2863e9c
Phillip Langley <philliplangley@champmbplangley.lan>
2026-04-14 14:45:29 -0500

    add extra sg for alb

--- outputs.tf
+++ outputs.tf
@@ -8,11 +8,6 @@ output "ecs_app_security_group" {
   value       = var.enabled ? aws_security_group.app[0].id : ""
 }
 
-output "alb_security_group" {
-  description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/security_group#id"
-  value       = var.enabled ? aws_security_group.alb[0].id : ""
-}
-
 output "execution_ecs_role_arn" {
   description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role#arn"
   value       = var.enabled ? aws_iam_role.this[0].arn : ""
@@ -50,25 +45,25 @@ output "lb_public_dns_name" {
 
 output "lb_private_dns_name" {
   description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lb#dns_name"
-  value       = var.enabled && !var.paused  ? aws_lb.private[0].dns_name : ""
+  value       = var.enabled && !var.paused ? aws_lb.private[0].dns_name : ""
 }
 
 output "lb_public_zone_id" {
   description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lb#zone_id"
-  value       = var.enabled && !var.paused  ? aws_lb.public[0].zone_id : ""
+  value       = var.enabled && !var.paused ? aws_lb.public[0].zone_id : ""
 }
 
 output "lb_private_zone_id" {
   description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lb#zone_id"
-  value       = var.enabled && !var.paused  ? aws_lb.private[0].zone_id : ""
+  value       = var.enabled && !var.paused ? aws_lb.private[0].zone_id : ""
 }
 
 output "lb_private_arn_suffix" {
   description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lb#arn_suffix"
-  value       = var.enabled && !var.paused  ? aws_lb.private[0].arn_suffix : ""
+  value       = var.enabled && !var.paused ? aws_lb.private[0].arn_suffix : ""
 }
 
 output "lb_public_arn_suffix" {
   description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lb#arn_suffix"
-  value       = var.enabled && !var.paused  ? aws_lb.public[0].arn_suffix : ""
+  value       = var.enabled && !var.paused ? aws_lb.public[0].arn_suffix : ""
 }
