a45f05f45ca9aca6c3525b6583f80990e4aecc63
Phillip Langley <phillip.langley@champtitles.com>
2026-08-24 09:25:12 -0500

    expose idle timeout

--- variables.tf
+++ variables.tf
@@ -121,3 +121,9 @@ variable "enable_waf" {
   type        = bool
   default     = false
 }
+
+variable "idle_timeout" {
+  description = "The idle timeout value for the load balancer"
+  type        = number
+  default     = 60
+}
