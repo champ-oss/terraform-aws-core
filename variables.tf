5abe7b09835465ea6339faffd687f51d1286b57d
Phillip Langley <phillip.langley@champtitles.com>
2026-08-24 10:01:05 -0500

    update old tf versions

--- variables.tf
+++ variables.tf
@@ -123,7 +123,7 @@ variable "enable_waf" {
 }
 
 variable "idle_timeout" {
-  description = "The idle timeout value for load balancers, in seconds. The default is 60 seconds."
+  description = "The idle timeout value for load balancers. The default is 60 seconds."
   type        = number
   default     = 60
 }
